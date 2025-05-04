2f4a7f2772d76410c9a64a266ea40e2d86d5cb49
Cristian Mitidieri <cristianmc100@gmail.com>
2025-05-04 15:03:49 -0300

    Project Completed

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -15,7 +15,7 @@ export const getUserData = async (req, res) => {
             return res.json({ success: false, message: "User not found" });
         }
 
-        req.json({ success: true, user });
+        res.json({ success: true, user });
     } catch (error) {
         res.json({ success: false, message: error.message });
     }
